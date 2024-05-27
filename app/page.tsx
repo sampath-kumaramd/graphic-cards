810eaf3b8f307d226074a55214757ef27b04dc5c
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-05-27 22:27:03 +0530

    create card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
+  const product={
+    id: 1,
+    image: 'https://ibb.co/jZpZHgR',
+    brand: 'MSI',
+    name: 'Nvidia RTX 4090 24GB',
+    description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+    price: 1024.99,
+    discount: 35,
+    deliveryTime: 1-2,
+    isLastDrop: true,
+    isLowestPrice: true,
+  }
   return (
     <Button>Click me</Button>
   );
---
+++ lib/enums/product-card-show.ts
@@ -0,0 +1,5 @@
+export enum ProductCardShow {
+    mini,
+    full,
+    branding,
+}
