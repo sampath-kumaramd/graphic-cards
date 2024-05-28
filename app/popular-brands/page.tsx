2e17a22b7ab2ce2b8b0f7f9689c02a467a266a2b
Sampath_Kumara <84168369+sampath-kumaramd@users.noreply.github.com>
2024-05-28 11:31:44 +0530

    complete brand page

---
+++ app/popular-brands/page.tsx
@@ -0,0 +1,55 @@
+import { brands } from '@/bin/band-page'
+import ProductCard from '@/components/product-card'
+import { ProductCardShow } from '@/lib/enums/product-card-show'
+import React from 'react'
+
+export default function PopularBrandPage() {
+
+  return (
+    <div className="max-w-screen-2xl 2xl:mx-auto mx-8 py-12 min-h-screen">
+      <div className=" font-bold text-3xl">Populaire merken </div>
+
+      <div className=' flex justify-start mt-4'>
+        MSI, een toonaangevend merk in de wereld van gaming en computerhardware,
+        staat bekend om zijn hoogwaardige grafische kaarten. Deze kaarten, ontworpen
+        voor zowel gamers als professionals, bieden uitstekende prestaties en betrouwbaarheid.
+        MSI&apos;s grafische kaarten zijn uitgerust met geavanceerde koelsystemen, zoals de Twin Frozr-serie,
+        die zorgen voor efficiënte warmteafvoer en stille werking. Met innovatieve functies zoals RGB-verlichting,
+        overklokmogelijkheden en robuuste bouwkwaliteit, leveren MSI grafische kaarten een optimale gamingervaring.
+        Daarnaast ondersteunt MSI regelmatig hun producten met software-updates en gebruikersvriendelijke tools,
+        wat bijdraagt aan hun reputatie als betrouwbare keuze voor pc-enthousiastelingen wereldwijd.
+      </div>
+      <div className=" grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 grid-cols-1 w-full 2xl:gap-24 xl:gap-8  items-center mt-8">
+        {brands.map((brand) => (
+          <ProductCard
+            id={brand.id}
+            key={brand.id}
+            image={brand.image}
+            brand={brand.brand}
+            showType={ProductCardShow.mini}
+            className=" mb-8 mx-auto"
+          />
+        ))}
+      </div>
+
+      <div className="my-20">
+        <div>
+          <div className="text-3xl font-bold mb-3">
+            Waarom kiezen voor een {'BRAND'}  grafische kaart?
+          </div>
+          <div className="text-xl text-black font-bold mt-8 ml-4">{'BRAND'} Grafische kaarten</div>
+          <div className="text-lg text-[#828282] font-medium mt-4 ml-4 ">
+            MSI, een toonaangevend merk in de wereld van gaming en computerhardware, staat bekend om zijn hoogwaardige
+            grafische kaarten. Deze kaarten, ontworpen voor zowel gamers als professionals, bieden uitstekende prestaties
+            en betrouwbaarheid. MSI&apos;s grafische kaarten zijn uitgerust met geavanceerde koelsystemen, zoals de Twin Frozr-serie,
+            die zorgen voor efficiënte warmteafvoer en stille werking. Met innovatieve functies zoals RGB-verlichting, overklokmogelijkheden
+            en robuuste bouwkwaliteit, leveren MSI grafische kaarten een optimale gamingervaring. Daarnaast ondersteunt MSI regelmatig hun
+            producten met software-updates en gebruikersvriendelijke tools, wat bijdraagt aan hun reputatie als betrouwbare keuze voor
+            pc-enthousiastelingen wereldwijd.
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
--- bin/band-page.ts
+++ bin/band-page.ts
@@ -1,10 +1,24 @@
 import brandImage from '../public/MSI.svg';
+import productImage from '../public/RTX.jpeg'
 
 interface Brands {
     id: number;
     image: string;
     brand: string;
 }
+
+interface Product {
+    id: number;
+    image: string;
+    brand: string;
+    name: string;
+    size: string;
+    description: string;
+    price: number;
+    discount: number;
+    deliveryTime: string;
+}
+
 export const brands: Brands[] = [
     {
         id: 1,
@@ -85,5 +99,184 @@ export const brands: Brands[] = [
         id: 16,
         image: brandImage.src,
         brand: 'MSI',
+    }
+]
+
+export const products: Product[] = [
+    {
+        id: 1,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 2,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 3,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 4,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 5,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 6,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 7,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 8,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 9,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 10,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 11,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 12,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 13,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 14,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 15,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
+    },
+    {
+        id: 16,
+        image: productImage.src,
+        brand: 'MSI',
+        name: 'Nvidia RTX 4090',
+        size: '24GB',
+        description: '2020 Apple MacBook Air Laptop: Apple M1 Chip, 13” Retina Display, 8GB RAM, 256GB SSD Storage, Backlit Keyboard, FaceTime HD Camera, Touch ID. Works with iPhone/iPad; Silver',
+        price: 1024.99,
+        discount: 35,
+        deliveryTime: "1 - 2",
     }
 ]
